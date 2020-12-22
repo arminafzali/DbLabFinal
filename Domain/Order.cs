81391901d1411d927622ece3635c488f227f167b
arminafzali <arminafzali23@gmail.com>
2020-12-22 16:39:40 +0330

    add some models

---
+++ Domain/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DbLabFinal.Domain
+{
+    public class Order : BaseEntity
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+    }
+}
